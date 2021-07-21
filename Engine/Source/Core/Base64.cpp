f4b48987d8b61bf053e69102d2a5992a8d202247
David Colson <davidcncolson@gmail.com>
2021-07-21 20:07:39 +0100

    Optimized base64 decoder, now 409 times faster!

--- Engine/Source/Core/Base64.cpp
+++ Engine/Source/Core/Base64.cpp
@@ -25,43 +25,68 @@ namespace An
         return out;
     }
 
-    static eastl::string lookup = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+   static const unsigned char encodingTable[65] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 
     // ***********************************************************************
 
     eastl::string DecodeBase64(eastl::string const& encodedString)
     {
-        if (encodedString.length() % 4 != 0)
+        unsigned char decodingTable[256];
+
+        for (size_t i = 0; i < sizeof(encodingTable) - 1; i++)
+		    decodingTable[encodingTable[i]] = (unsigned char) i;
+	    decodingTable['='] = 0;
+
+        const char* input = encodedString.data();
+        size_t inputLength = encodedString.length();
+
+        size_t count = 0;
+        for (int i = 0; i < inputLength; i++) {
+            if (decodingTable[input[i]] != 0x80)
+                count++;
+        }
+
+        if (count == 0 || count % 4)
         {
             Log::Crit("Invalid base64 encoded string");
-            return "";
+            return NULL;
         }
 
-        eastl::string output;
-        output.reserve(encodedString.length());
-        for( int i= 0; i < encodedString.length(); i += 4)
+        size_t outputlen = count / 4 * 3;
+    
+        char* output = new char[outputlen];
+        char* position = output;
+
+        for( int i= 0; i < inputLength; i += 4)
         {
-            char a = lookup.find(encodedString[i]) & 0xFF;
-            char b = lookup.find(encodedString[i + 1]) & 0xFF;
-            char c = lookup.find(encodedString[i + 2]) & 0xFF; 
-            char d = lookup.find(encodedString[i + 3]) & 0xFF; 
+            char a = decodingTable[input[i]] & 0xFF;
+            char b = decodingTable[input[i + 1]] & 0xFF;
+            char c = decodingTable[input[i + 2]] & 0xFF; 
+            char d = decodingTable[input[i + 3]] & 0xFF; 
 
-            output += a << 2 | (b & 0x30) >> 4;
+            *position++ = a << 2 | (b & 0x30) >> 4;
             if (c != 0x40)
-                output += b << 4 | (c & 0x3c) >> 2;
+                *position++ = b << 4 | (c & 0x3c) >> 2;
             if (d != 0x40)
-                output += (c << 6 | d);
+                *position++ = (c << 6 | d);
         }
 
-        return output;
+        eastl::string result(output, output + outputlen);
+        delete output;
+        return result;
     }
 
     // ***********************************************************************
 
     eastl::string EncodeBase64(size_t length, const char* bytes)
     {
-        eastl::string output;
-        output.reserve(length * 2);
+        size_t outputLength = length * 4 / 3 + 4; // 3-byte blocks to 4-byte
+        outputLength++; // nul termination
+        if (outputLength < length)
+            return NULL; // integer overflow
+        char* output = new char[outputLength];
+        char* position = output;
+
         for( int i= 0; i < length; i += 3)
         {
             int nChars = (int)length - i;
@@ -69,12 +94,14 @@ namespace An
             char b = nChars == 1 ? 0xF : bytes[i + 1];
             char c = nChars < 3 ? 0x3F : bytes[i + 2]; 
             
-            output += lookup[a >> 2];
-            output += lookup[(a & 0x3) << 4 | b >> 4];
-            output += lookup[b == 0xF ? 0x40 : (b & 0xF) << 2 | c >> 6];
-            output += lookup[c == 0x3F ? 0x40 : (c & 0x3F)];
+            *position++ = encodingTable[a >> 2];
+            *position++ = encodingTable[(a & 0x3) << 4 | b >> 4];
+            *position++ = encodingTable[b == 0xF ? 0x40 : (b & 0xF) << 2 | c >> 6];
+            *position++ = encodingTable[c == 0x3F ? 0x40 : (c & 0x3F)];
         }
 
-        return output;
+        eastl::string result(output, output + outputLength);
+        delete output;
+        return result;
     }
 }
